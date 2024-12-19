c5400118c810a8f2c04d9490670e03fed9793b8f
mpat519 <mpat519@wgu.edu>
2024-12-18 19:53:20 -0800

    working_branch created and initial commit

---
+++ src/main/java/com/d288/mit/MitApplication.java
@@ -0,0 +1,13 @@
+package com.d288.mit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MitApplication.class, args);
+    }
+
+}
